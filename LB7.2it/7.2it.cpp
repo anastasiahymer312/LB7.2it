cc3caedc42beeef6dcc79021dd774b9594c50840
anastasiahymer312 <anastasiagumer312@gmail.com>
2024-10-19 15:57:35 +0300

    Add files via upload

---
+++ LB7.2it/7.2it.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High);
+void Print(int** a, const int rowCount, const int colCount);
+void findSumOfMaxInEvenRows(int** a, const int rowCount, const int colCount);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    srand((unsigned)time(NULL));
+
+    int Low = -34;
+    int High = 26;
+    int rowCount;
+    int colCount;
+    cout << "k = "; cin >> rowCount;
+    cout << "n = "; cin >> colCount;
+
+    int** a = new int* [rowCount];
+
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+
+    Create(a, rowCount, colCount, Low, High);
+    Print(a, rowCount, colCount);
+
+    findSumOfMaxInEvenRows(a, rowCount, colCount);
+    Print(a, rowCount, colCount);
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High)
+{
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < colCount; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int rowCount, const int colCount)
+{
+    cout << endl;
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void findSumOfMaxInEvenRows(int** a, const int rowCount, const int colCount)
+{
+    int sum = 0;
+
+    for (int i = 0; i < rowCount; i += 2)
+    {
+        int max = a[i][0];
+
+        for (int j = 0; j < colCount; j++)
+        {
+            if (a[i][j] > max)
+            {
+                max = a[i][j];
+            }
+        }
+
+        sum += max;
+    }
+
+    cout << "Сума максимальних елементів по парних рядках: " << sum << endl;
+}
